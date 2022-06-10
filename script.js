a63ac4bb30000ec991efe8a926e685c45ef37ee6
pavanchambhare <pavanchambhare88@gmail.com>
2022-06-10 19:49:29 +0530

    first commit

--- script.js
+++ script.js
@@ -6,6 +6,39 @@
 
 // console.log(c)
 
-let d = prompt('Please enter tha number', '');
-let e = Math.sqrt(d)
-console.log(Math.pow(e , 2));
+// let d = prompt('Please enter tha number', '');
+// let e = Math.sqrt(d)
+// console.log(Math.pow(e , 2));
+
+// find the area of right angled trangle
+
+// general formula:(base * hight)/2
+
+// let base = prompt('please enter base and hight')
+// let area = ()
+
+
+// for (var a =1; a<=100; a= a+10){
+//     for(var b = 1; b <= a ;  b++)
+//     document.write(b + " ");
+// }
+// document.write("<br>")
+
+
+
+// let a = confirm("Do You Like Website");
+
+// if(a){
+//     alert("thanks");
+// }
+// else{
+//   alert("opss")
+// }
+
+let a = prompt("What Your Name");
+//  let b = prompt(age);
+
+document.write(a);
+// document.write(b);
+
+
